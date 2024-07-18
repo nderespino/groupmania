7afaef26a61571af1b1de79c97d815e7a3cdf35b
Nicolette <nderespino@gmail.com>
2024-07-18 16:11:36 -0400

    initial upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import NavbarComp from './components/NavbarComp'
+import CardComp from './components/CardComp'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+
+  return (
+    <div className ="App">
+     <NavbarComp/>
+     <CardComp/>
+    </div>
+  )
+}
+
+export default App;
